ecf34863a7e464f79cb07f91888a932093c79f97
Roghit <roghitv2003@gmail.com>
2026-06-15 16:55:08 +0530

    Get and Post mapping in MongoDB Atlas

---
+++ src/main/java/com/launchPad/jobs/controller/PostController.java
@@ -0,0 +1,41 @@
+package com.launchPad.jobs.controller;
+
+import com.launchPad.jobs.model.Post;
+import com.launchPad.jobs.repository.PostRepo;
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    PostRepo repo;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @RequestMapping("/swagger")
+    @Hidden  // ← hides this endpoint from Swagger UI
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
+    }
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts() {
+        System.out.println(">>> DB NAME = " + mongoTemplate.getDb().getName());
+        System.out.println(">>> COLLECTION EXISTS = " + mongoTemplate.collectionExists(Post.class));
+        System.out.println(">>> COUNT = " + mongoTemplate.count(new org.springframework.data.mongodb.core.query.Query(), Post.class));
+        return repo.findAll();
+    }
+
+    @PostMapping("/post")
+    public Post addPost(@RequestBody Post post){
+        return repo.save(post);
+    }
+}
---
+++ src/main/java/com/launchPad/jobs/repository/PostRepo.java
@@ -0,0 +1,7 @@
+package com.launchPad.jobs.repository;
+
+import com.launchPad.jobs.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostRepo extends MongoRepository<Post,String> {
+}
